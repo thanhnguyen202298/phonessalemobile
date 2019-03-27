cf063a1716893b7da576e8a1f6f420e955fa07e1
thanh nguyễn trung <victo202298@gmail.com>
2019-03-27 16:23:46 +0700

    item list imagetext

--- app/src/main/java/com/aln/phonesaleschain/screen/order/detail/DetailCartActivity.java
+++ app/src/main/java/com/aln/phonesaleschain/screen/order/detail/DetailCartActivity.java
@@ -2,11 +2,8 @@ package com.aln.phonesaleschain.screen.order.detail;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +11,7 @@ import android.view.ViewGroup;
 
 import com.aln.phonesaleschain.R;
 import com.aln.phonesaleschain.databinding.ActivityDetailCartBinding;
-import com.aln.phonesaleschain.screen.newsspaper.ProductActivity;
+import com.aln.phonesaleschain.screen.fragment_itemlist.ProductActivity;
 
 public class DetailCartActivity extends Fragment {
     private static final String ARG_PARAM1 = "param1";
