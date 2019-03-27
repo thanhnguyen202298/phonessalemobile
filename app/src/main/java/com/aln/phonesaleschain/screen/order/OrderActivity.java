97a130ad5d089a259d80a22b15f21f6ce4a49116
thanh nguyễn trung <victo202298@gmail.com>
2019-03-27 14:52:25 +0700

    add order

---
+++ app/src/main/java/com/aln/phonesaleschain/screen/order/OrderActivity.java
@@ -0,0 +1,6 @@
+package com.aln.phonesaleschain.screen.order;
+
+import android.support.v4.app.Fragment;
+
+public class OrderActivity extends Fragment {
+}
---
+++ app/src/main/java/com/aln/phonesaleschain/screen/order/detail/DetailCartActivity.java
@@ -0,0 +1,104 @@
+package com.aln.phonesaleschain.screen.order.detail;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aln.phonesaleschain.R;
+import com.aln.phonesaleschain.databinding.ActivityDetailCartBinding;
+import com.aln.phonesaleschain.screen.newsspaper.ProductActivity;
+
+public class DetailCartActivity extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private int mParam2;
+    private ActivityDetailCartBinding fragNews;
+
+    private DetailCartActivity.OnFragmentInteractionListener mListener;
+
+    public DetailCartActivity() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param fragname Parameter 1.
+     * @param orient Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProductActivity newInstance(String fragname, int orient) {
+        ProductActivity fragment = new ProductActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, fragname);
+        args.putInt(ARG_PARAM2, orient);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getInt(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        fragNews = DataBindingUtil.inflate(inflater,R.layout.activity_detail_cart,container,false);
+//        initialize();
+
+        return fragNews.getRoot();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof ProductActivity.OnFragmentInteractionListener) {
+            mListener = (DetailCartActivity.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+
+//        loadData();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
