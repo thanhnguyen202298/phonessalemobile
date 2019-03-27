ef20636af99646528c92a0d7a28e1365ad03c33a
thanh nguyễn trung <victo202298@gmail.com>
2019-03-27 17:30:07 +0700

    item list imagetext

--- app/src/main/java/com/aln/phonesaleschain/customview/ItemVariable.java
+++ app/src/main/java/com/aln/phonesaleschain/customview/ItemVariable.java
@@ -3,14 +3,22 @@ package com.aln.phonesaleschain.customview;
 import android.view.View;
 
 import com.aln.phonesaleschain.adapter.MyAdapter;
+import com.google.gson.annotations.SerializedName;
 
 public class ItemVariable {
     public int idDrawer;
     public int idString;
+
+    @SerializedName("name")
     public String label;
 
+    public String amount = "";
 
-    public void ClickMe(View view){
+    public String getLabel() {
+        return label + " " + amount;
+    }
+
+    public void ClickMe(View view) {
         MyAdapter.onClick(idDrawer);
     }
 }
